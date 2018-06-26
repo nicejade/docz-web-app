13dbe64d2e1dd803cb8df391d7cf2ffe7785b440
nicejade <jeffygisgreat@gmail.com>
2018-06-27 01:41:48 +0800

    📝  Update Deploy gitignore  Etc.

--- .docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'home/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "home-index" */ 'home/index.mdx'),
-  'works/pages/docz/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "works-pages-docz-index" */ 'works/pages/docz/index.mdx'),
-  'works/pages/vuepress/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "works-pages-vuepress-index" */ 'works/pages/vuepress/index.mdx'),
-}
